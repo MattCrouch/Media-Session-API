6672897880f3dd7e061888911e89be448a3b1b03
Matt Crouch <github@mattcrouch.net>
2017-03-12 20:50:42 +0000

    Convert fetch to xhr
    Let Safari join the party

--- static/js/main.js
+++ static/js/main.js
@@ -5,14 +5,17 @@
     playlist.subscribe(player);
     player.subscribe(playlist);
 
-    fetch("/podcasts")
-        .then(resp => resp.json())
-        .then(resp => {
-            resp.forEach(function(item) {
-                let playlistItem = new PlaylistItem(item);
-                playlist.add(playlistItem);
-            }, this);
+    let getPodcasts = new XMLHttpRequest();
+    getPodcasts.open("GET", "/podcasts");
+    getPodcasts.addEventListener("load", function() {
+        let podcasts = JSON.parse(this.responseText);
+        podcasts.forEach(function(item) {
+            let playlistItem = new PlaylistItem(item);
+            playlist.add(playlistItem);
+        }, this);
 
-            player.setActiveItem(playlist.getActiveItem());
-        });
+        player.setActiveItem(playlist.getActiveItem());
+    });
+
+    getPodcasts.send();
 })();
